493c93516f788698b75258df0356c67d5ed42b82
Sebastián Santillán <sebastian.santillan@gmail.com>
2024-02-12 00:08:29 -0300

    Se actualiza componente VTextArea

---
+++ resources/js/Components/VTextArea.vue
@@ -0,0 +1,8 @@
+<template>
+    <textarea class="block w-full border border-gray-300 text-md rounded-md py-2.5 text-gray-900 ring-gray-300 placeholder:text-gray-400 focus:bg-zinc-50 focus:ring-blue-300"
+    v-model="modelValue"
+    ></textarea>
+</template>
+<script setup lang="ts">
+const modelValue = defineModel<string|number| readonly string[]>();
+</script>
--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -25,16 +25,44 @@
                 <v-input/>
             </div>            
         </div>
+        <div class="mt-2 grid gap-x-6 gap-y-2 sm:grid-cols-6 lg:grid-cols-12">
+            <div class="sm:col-span-2 lg:col-span-4">
+                <v-label>Label 4</v-label>
+                <v-select>
+                    <option value="">Item 1</option>
+                    <option value="">Item 2</option>                    
+                </v-select>
+            </div>
+            <div class="sm:col-span-2 lg:col-span-4">
+                <v-label>Label 2</v-label>
+                <v-input/>
+            </div>
+            <div class="sm:col-span-2 lg:col-span-4">
+                <v-label>Label 3</v-label>
+                <v-input/>
+            </div>            
+        </div>  
+        <div class="mt-2 grid gap-x-6 gap-y-2 sm:grid-cols-6 lg:grid-cols-12">
+            <div class="sm:col-span-2 lg:col-span-4">
+                <v-label>TextArea 5</v-label>
+                <VTextArea v-model="comentario"/>
+            </div>                     
+        </div>                
         <div class="mt-6">
             <VButtonPrimary>Guardar</VButtonPrimary>
         </div>
     </AppLayout>
 </template>
 <script setup lang="ts">
+import { ref } from 'vue';
 import AppLayout from '@/Layouts/AppLayout.vue';
 import VH1 from '@/Components/VH1.vue';
 import VLabel from '@/Components/VLabel.vue'
 import VInput from '@/Components/VInput.vue'
+import VSelect from '@/Components/VSelect.vue'
+import VTextArea from '@/Components/VTextArea.vue'
 import VButtonPrimary from '@/Components/VButtonPrimary.vue'
+
+const comentario = ref('Este es un comentario')
 </script>
 
